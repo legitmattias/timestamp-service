373e150c04b302294d7507a90027dad25bc93800
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-17 22:36:49 +0100

    Refactor: Add asynchandler util and basic error handler in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import noteRoutes from './routes/noteRoutes'
 
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use('/notes', noteRoutes)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ error: err.message || "Internal Server Error" });
-});
+app.use((err: Error, req: Request, res: Response) => {
+  console.error(err.stack)
+  res.status(500).json({ error: err.message || 'Internal Server Error' })
+})
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,11 +1,11 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express'
 
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 
 const asyncHandler = (fn: AsyncHandler) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        fn(req, res, next).catch(next);
-    };
-};
+        fn(req, res, next).catch(next)
+    }
+}
 
-export default asyncHandler;
+export default asyncHandler
