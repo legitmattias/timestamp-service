c143f16ab75005d46577d7e80d4f051f35746477
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-01-18 00:01:55 +0100

    Refactor: Remove debug logging.

--- scripts/deploy.cjs
+++ scripts/deploy.cjs
@@ -1,18 +1,16 @@
-const { ethers } = require("hardhat");
+const { ethers } = require("hardhat")
 
 async function main () {
-  console.log("Getting contract factory...");
-  const TimestampStorage = await ethers.getContractFactory("TimestampStorage");
+  console.log("Getting contract factory...")
+  const TimestampStorage = await ethers.getContractFactory("TimestampStorage")
 
   console.log("Deploying contract...");
-  const timestampStorage = await TimestampStorage.deploy(); // Deploy the contract
+  const timestampStorage = await TimestampStorage.deploy()
 
-  // Use the 'target' property for the contract address
-  console.log("Contract deployment result:", timestampStorage);
-  console.log("TimestampStorage deployed to:", timestampStorage.target);
+  console.log("TimestampStorage deployed to:", timestampStorage.target)
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+  console.error(error)
+  process.exitCode = 1
 });
